b8e1a4002c0ab5cfbd284d08828652b81431e95c
dnz3d4c <advck1123@gmail.com>
2020-12-28 09:27:37 +0900

    변수 이름을 알아보기 쉽도록 바꿉니다

--- addon/globalPlugins/multiTaskingWindowNotifier/__init__.py
+++ addon/globalPlugins/multiTaskingWindowNotifier/__init__.py
@@ -24,8 +24,8 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	# 추가 기능 기본 변수
 	configPath = globalVars.appArgs.configPath
-	global appListFile
-	appListFile = os.path.join(configPath, "addons", "multiTaskingWindowNotifier", "globalPlugins", "multiTaskingWindowNotifier") + "\\app.list"
+	global APPListFile
+	APPListFile = os.path.join(configPath, "addons", "multiTaskingWindowNotifier", "globalPlugins", "multiTaskingWindowNotifier") + "\\app.list"
 
 	# 창 목록 추가를 위한 제스처/함수
 	@script(
@@ -34,7 +34,7 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	gesture = ("kb:NVDA+Shift+t")
 	)
 	def script_addListWindow(self, gesture):
-		with open(appListFile, "a", encoding="utf8") as f:
+		with open(APPListFile, "a", encoding="utf8") as f:
 			fg = api.getForegroundObject()
 			add = f.write(fg.name + "\n")
 		ui.message("추가됨")
@@ -44,11 +44,11 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		obj = api.getFocusObject()
 		if obj.windowClassName == "MultitaskingViewFrame":
 			# APP 목록 파일 열기
-			with open(appListFile, "r", encoding="utf8") as f:
-				appList = f.readlines()
+			with open(APPListFile, "r", encoding="utf8") as f:
+				APPList = f.readlines()
 			# 앱 목록에서 각 항목의 \N문자 제거
-			for i in range(len(appList)):
-				text = appList[i].strip("\n")
+			for i in range(len(APPList)):
+				text = APPList[i].strip("\n")
 				# 앱 항목과 초점 객체가 일치하면 앱 항목에 해당하는 비프음 출력
 				if obj.name == text:
 					beepList = [
